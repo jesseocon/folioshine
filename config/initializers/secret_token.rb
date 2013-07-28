c85e44d5a2209d2b6ba0b5184d60893113926af9
jesseocon <jesse.ocon@oconcompany.com>
2013-07-28 12:12:18 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Folioshine::Application.config.secret_token = '8652cbe857be49593d69fa6e6649923849767c7e272d8bdf9cd4971a28671161152b4c4506f76fcf1a078ab6d72d246c38fb0f7343a6f6345a5fd6e7b3dd91fc'
